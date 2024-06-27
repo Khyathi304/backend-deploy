6ab98d0a82023a53cf24d2a75e7941ee34c3f8a8
Khyathi304 <khyathikamireddy04@gmail.com>
2024-06-27 14:12:29 +0100

    expense

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,15 +32,12 @@ resource "null_resource" "backend" {
       
     }
 
-    provisioner "remote-exec" {
-        inline = [ 
+   provisioner "remote-exec" {
+        inline = [
             "chmod +x /tmp/${var.common_tags.Component}.sh",
             "sudo sh /tmp/${var.common_tags.Component}.sh ${var.common_tags.Component} ${var.environment} ${var.app_version}"
-
-         ]
-      
-    }
-
+        ]
+    } 
 }
 
 resource "aws_ec2_instance_state" "backend" {
@@ -164,7 +161,7 @@ resource "aws_autoscaling_policy" "backend" {
       predefined_metric_type = "ASGAverageCPUUtilization"
     }
 
-    target_value = 10.0
+    target_value = 80.0
   }
 }
 
